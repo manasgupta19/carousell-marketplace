50361816603dd4ceac3d3cf0c3c0812168e9d1a4
manasgupta19 <52977428+manasgupta19@users.noreply.github.com>
2026-02-05 14:57:47 +0530

    Delete src directory

--- src/main/java/com/carousell/marketplace/command/sort/ListingSortStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.carousell.marketplace.command.sort;
-
-import com.carousell.marketplace.model.Listing;
-import java.util.Comparator;
-
-public interface ListingSortStrategy {
-    Comparator<Listing> getComparator();
-}
